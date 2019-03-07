f2ed7fe183892274395338970cf2328413ec7f3b
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-07 17:28:31 -0500

    Carousel Component Stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,52 @@
 class Carousel {
+  constructor(element) {
+    this.element = element;
 
+    this.leftButton = this.element.querySelector(".left-button");
+    this.leftButton.addEventListener("click", () => {
+      this.selectLeft();
+    });
+
+    this.rightButton = this.element.querySelector(".right-button");
+    this.rightButton.addEventListener("click", () => {
+      this.selectRight();
+    });
+
+    this.imgs = this.element.querySelectorAll(".carousel-img");
+    Array.from(this.imgs);
+
+    this.position = 0;
+    this.showing = this.imgs[this.position].classList.add("show");
+  }
+
+  selectLeft() {
+    this.imgs.forEach(function(img) {
+      img.classList.remove("show");
+    });
+
+    if (this.position === 0) {
+      this.position = 3;
+    } else {
+      this.position = this.position - 1;
+    }
+
+    this.showing = this.imgs[this.position].classList.add("show");
+  }
+
+  selectRight() {
+    this.imgs.forEach(function(img) {
+      img.classList.remove("show");
+    });
+
+    if (this.position === 3) {
+      this.position = 0;
+    } else {
+      this.position = this.position + 1;
+    }
+
+    this.showing = this.imgs[this.position].classList.add("show");
+  }
 }
 
-let carousel = document.querySelector();
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
+let carousel = document.querySelector(".carousel");
+carousel = new Carousel(carousel);
